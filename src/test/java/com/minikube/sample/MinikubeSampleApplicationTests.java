69a293683dc7f78c220c599799d9645863a9c1f8
sumanthtony <sumanthbattagiri99@gmail.com>
2026-02-07 16:54:48 +0530

    Update MinikubeSampleApplicationTests.java

--- src/test/java/com/minikube/sample/MinikubeSampleApplicationTests.java
+++ src/test/java/com/minikube/sample/MinikubeSampleApplicationTests.java
@@ -1,16 +1,23 @@
 package com.minikube.sample;
 
+import static org.junit.Assert.assertNotNull;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MinikubeSampleApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private ApplicationContext applicationContext;
 
+    @Test
+    public void contextLoads() {
+        assertNotNull(applicationContext);
+    }
 }
